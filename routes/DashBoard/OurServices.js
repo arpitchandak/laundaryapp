da4c0d603ff3c1df31bb9003cbb77cd22aa124dc
Arpit Chandak <arpitchandak123@gmail.com>
2020-07-08 17:49:45 +0530

    final

--- routes/DashBoard/OurServices.js
+++ routes/DashBoard/OurServices.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 
 
 router.get('/service_type',async (req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
 
     try {
         const result = await Product.find({} , {__v: 0})
@@ -24,6 +25,7 @@ router.get('/service_type',async (req,res,next) => {
 })
 
 router.post('/service_type',async (req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
 
     try {
         await Product.find({service_name: req.body.service_name})
@@ -51,6 +53,7 @@ router.post('/service_type',async (req,res,next) => {
 
 
 router.delete('/service_type/:id',async (req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
 
     const id = req.params.id
     try {
@@ -67,6 +70,7 @@ router.delete('/service_type/:id',async (req,res,next) => {
 
 
 router.patch('/service_type/:id',async (req,res,next) => {
+       res.setHeader('Access-Control-Allow-Origin','*');
 
     try {
         const id = req.params.id
